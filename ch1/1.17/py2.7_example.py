b2b7c3a982f4f31fbc44b96a0e97033151ba054f
Lawrence R. Amlord <informlarry@gmail.com>
2016-08-06 14:43:31 +0800

    add section 1.17

---
+++ ch1/1.17/py2.7_example.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+PROBLEM
+
+You want to make a dictionary that is a subset of another dictionary.
+"""
+
+
+"""
+SOLUTION
+"""
+
+prices = {
+    'ACME': 45.23,
+    'AAPL': 612.78,
+    'IBM': 205.55,
+    'HPQ': 37.20,
+    'FB': 10.75
+}
+
+# Make a dictionary of all prices over 200
+p1 = { key:value for key, value in prices.items() if value > 200 }
+print(p1)
+
+# Make a dictionary of tech stocks
+tech_names = { 'AAPL', 'IBM', 'HPQ', 'MSFT' }
+p2 = { key:value for key, value in prices.items() if key in tech_names}
+print(p2)
+
+
+"""
+DISCUSSION
+"""
+
+p1 = dict((key, value) for key, value in prices.items() if value > 200)
+print(p1)
+
+# Make a dictionary of tech stocks
+tech_names = { 'AAPL', 'IBM', 'HPQ', 'MSFT' }
+p2 = { key:prices[key] for key in prices.viewkeys() & tech_names}
+print(p2)
