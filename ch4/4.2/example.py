0e697fbdf7f2463b6c664596a60cc2216d9ede38
Lawrence R. Amlord <informlarry@gmail.com>
2016-08-13 12:00:34 +0800

    add section 4.2

---
+++ ch4/4.2/example.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+PROBLEM
+
+You have built a custom container object that internally holds a list, tuple, or
+some other iterable. You would like to make iteration work with your new
+container.
+"""
+
+
+"""
+SOLUTION
+"""
+
+
+class Node:
+    def __init__(self, value):
+        self._value = value
+        self._children = []
+
+    def __repr__(self):
+        return 'Node({!r})'.format(self._value)
+
+    def add_child(self, node):
+        self._children.append(node)
+
+    def __iter__(self):
+        return iter(self._children)
+
+
+# Example
+if __name__ == '__main__':
+    root = Node(0)
+    child1 = Node(1)
+    child2 = Node(2)
+    root.add_child(child1)
+    root.add_child(child2)
+    for ch in root:
+        print(ch)
+    # Outputs Node(1), Node(2)
