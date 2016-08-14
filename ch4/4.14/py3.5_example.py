263c147f2d9a2ed2fafab5157787b18e11bd8bda
Lawrence R. Amlord <informlarry@gmail.com>
2016-08-14 22:28:36 +0800

    add section 4.14

---
+++ ch4/4.14/py3.5_example.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+PROBLEM
+
+You have a nested sequence that you want to flatten into a single list of values.
+"""
+
+
+"""
+SOLUTION
+"""
+
+from collections import Iterable
+
+def flatten(items, ignore_types=(str, bytes)):
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            yield from flatten(x)
+        else:
+            yield x
+
+items = [1, 2, [3, 4, [5, 6], 7], 8]
+
+# Produces 1 2 3 4 5 6 7 8
+for x in flatten(items):
+    print(x)
+
+items = ['Dave', 'Paula', ['Thomas', 'Lewis']]
+
+for x in flatten(items):
+    print(x)
+
+
+"""
+DISCUSSION
+"""
+
+def flatten(items, ignore_types=(str, bytes)):
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            for i in flatten(x):
+                yield i
+        else:
+            yield x
+
+items = [1, 2, [3, 4, [5, 6], 7], 8]
+
+# Produces 1 2 3 4 5 6 7 8
+for x in flatten(items):
+    print(x)
