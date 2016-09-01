63cd297d0b219b3c7b603fc14ccdc550ae9ae384
Lawrence R. Amlord <informlarry@gmail.com>
2016-09-01 17:07:33 +0800

    add section 9.24

---
+++ ch9/9.24/namelower.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+PROBLEM
+
+You want to write programs that parse and analyze Python source code.
+"""
+
+
+"""
+DISCUSSION
+"""
+
+import ast
+import inspect
+
+
+# Node visitor that lowers globally accessed names into
+# the function body as local variables.
+class NameLower(ast.NodeVisitor):
+
+    def __init__(self, lowered_names):
+        self.lowered_names = lowered_names
+
+    def visit_FunctionDef(self, node):
+        # Compile some assignments to lower the constants
+        code = '__globals = globals()\n'
+        code = code + '\n'.join("{0} = __globals['{0}']".format(name)
+                                for name in self.lowered_names)
+
+        code_ast = ast.parse(code, mode='exec')
+
+        # Inject new statements into the function body
+        node.body[:0] = code_ast.body
+
+        # Save the function object
+        self.func = node
+
+
+# Decorator that turns global names into locals
+def lower_names(*namelist):
+    def lower(func):
+        srclines = inspect.getsource(func).splitlines()
+        # Skip source lines prior to the @lower_names decorator
+        for n, line in enumerate(srclines):
+            if '@lower_names' in line:
+                break
+
+        src = '\n'.join(srclines[n+1:])
+
+        # Hack to deal with indented code
+        if src.startswith((' ', '\t')):
+            src = 'if 1:\n' + src
+        top = ast.parse(src, mode='exec')
+
+        # Transform the AST
+        cl = NameLower(namelist)
+        cl.visit(top)
+
+        # Execute the modified AST
+        temp = {}
+        exec(compile(top, '', 'exec'), temp, temp)
+
+        # Pull out the modified code object
+        func.__code__ = temp[func.__name__].__code__
+        return func
+    return lower
+
+
+# Example of use
+INCR = 1
+
+def countdown1(n):
+    while n > 0:
+        n = n - INCR
+
+@lower_names('INCR')
+def countdown2(n):
+    while n > 0:
+        n = n - INCR
+
+
+if __name__ == '__main__':
+    import time
+    print('Running a performance check')
+
+    start1 = time.time()
+    countdown1(100000000)
+    end1 = time.time()
+    elapse1 = end1 - start1
+    print('countdown1: ', elapse1)
+
+    start2 = time.time()
+    countdown2(100000000)
+    end2 = time.time()
+    elapse2 = end2 - start2
+    print('countdown2: ', elapse2)
